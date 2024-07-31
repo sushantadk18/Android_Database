bdbcee965cab81cdc0593e41f856fcf90aa41756
Sushant Adhikari <sushantadk18@gmail.com>
2024-07-31 07:59:09 +0545

    updated code

--- app/src/main/java/com/example/android_database/DatabaseHelper.java
+++ app/src/main/java/com/example/android_database/DatabaseHelper.java
@@ -9,57 +9,66 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+
     public static final String DATABASE_NAME="student.db";
-    public static final String TABLE_NAME="student.table";
+
+    public static final String TABLE_NAME="student_table";
+
     public static final String COL1="id";
+
     public static final String COL2="name";
+
     public static final String COL3="marks";
-    public DatabaseHelper(Context context) {
+
+
+    public DatabaseHelper( Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(" create table " + TABLE_NAME +
-                 "(ID INTEGER PRIMARY KEY AUTOINCREMENT , NAME TEXT ,MARKS INTEGER)");
+        sqLiteDatabase.execSQL(" create table "+ TABLE_NAME+
+                "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT,MARKS INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-    sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS "+TABLE_NAME);
-    onCreate(sqLiteDatabase);
+
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+
     }
-    public boolean insertData(String id ,String name,String marks){
+    public boolean insertData(String id, String name, String marks){
+
         SQLiteDatabase sqLiteDatabase=this.getWritableDatabase();
         ContentValues contentValues=new ContentValues();
-        contentValues.put(COL2,name);
-        contentValues.put(COL3,marks);
-        Long result=sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
-        if (result==1){
+        contentValues.put(COL2, name);
+        contentValues.put(COL3, marks);
+        Long result=sqLiteDatabase.insert(TABLE_NAME, null,contentValues);
+        if (result==-1){
             return false;
-
-        }
-        else {
+        }else {
             return true;
         }
     }
     public Cursor getAllData(){
         SQLiteDatabase sqLiteDatabase=this.getWritableDatabase();
-        Cursor cursor=sqLiteDatabase.rawQuery("select * from " +TABLE_NAME,null);
-        return  cursor;
+        Cursor cursor=sqLiteDatabase.rawQuery(" Select * from "+ DatabaseHelper.TABLE_NAME, null);
+        return cursor;
     }
+
     public boolean updateData(String id, String name, String marks){
         SQLiteDatabase sqLiteDatabase=this.getWritableDatabase();
         ContentValues contentValues=new ContentValues();
         contentValues.put(COL1,id);
         contentValues.put(COL2,name);
         contentValues.put(COL3,marks);
-        sqLiteDatabase.update(TABLE_NAME,contentValues,"ID=?", new String[]{id});
+        sqLiteDatabase.update(TABLE_NAME,contentValues, "ID=?", new String[]{id});
         return true;
     }
     public Integer deleteData(String id){
-    SQLiteDatabase sqLiteDatabase=this.getWritableDatabase();
-    return sqLiteDatabase.delete(TABLE_NAME,"ID=?",new String[]{id});
+        SQLiteDatabase sqLiteDatabase=this.getWritableDatabase();
+        return sqLiteDatabase.delete(TABLE_NAME,"ID=?", new String[]{id});
     }
 
 }
--- app/src/main/java/com/example/android_database/MainActivity.java
+++ app/src/main/java/com/example/android_database/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.android_database;
 
+import static android.app.ProgressDialog.show;
+
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.icu.text.CaseMap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,35 +18,101 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    EditText txt_id,txt_name,txt_marks;
-    Button btn_insertData,btn_viewData,btn_editData,btn_deleteData;
+
+
+    EditText txt_id, txt_name, txt_marks;
+    Button btn_insert, btn_view, btn_edit, btn_delete;
     DatabaseHelper myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         txt_id=findViewById(R.id.editId);
-        txt_name=findViewById(R.id.editMarks);
+        txt_name=findViewById(R.id.editName);
         txt_marks=findViewById(R.id.editMarks);
-        btn_insertData=findViewById(R.id.btn_insert);
-        btn_viewData=findViewById(R.id.btn_view);
-        btn_editData=findViewById(R.id.btn_edit);
-        btn_deleteData=findViewById(R.id.btn_delete);
+
+        btn_insert=findViewById(R.id.btn_insert);
+        btn_view=findViewById(R.id.btn_view);
+        btn_edit=findViewById(R.id.btn_edit);
+        btn_delete=findViewById(R.id.btn_delete);
+
         myDB=new DatabaseHelper(this);
-        btn_insertData.setOnClickListener(new View.OnClickListener() {
+
+        btn_insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean inserted =myDB.insertData(txt_id.getText().toString().trim(),
+                boolean inserted=myDB.insertData(
+                        txt_id.getText().toString(),
+                        txt_name.getText().toString(),
+                        txt_marks.getText().toString());
+                if (inserted==true){
+                    Toast.makeText(MainActivity.this, "Data has Inserted", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "Something error", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        btn_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor=myDB.getAllData();
+                if(cursor.getCount()==0){
+                    Toast.makeText(MainActivity.this, "Empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                StringBuffer buffer=new StringBuffer();
+                while  (cursor.moveToNext()){
+                    buffer.append("Id: "+cursor.getString(0)+"\n");
+                    buffer.append("Name: "+cursor.getString(1)+"\n");
+                    buffer.append("Marks: "+cursor.getString(2)+"\n");
+                }
+                show("Data",buffer.toString());
+            }
+        });
+
+        btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isUpdate=myDB.updateData(
+                        txt_id.getText().toString().trim(),
                         txt_name.getText().toString().trim(),
                         txt_marks.getText().toString().trim());
-                if (inserted==true){
-                    Toast.makeText(MainActivity.this,"data has beeen insertted",Toast.LENGTH_SHORT).show();
+                if(isUpdate==true){
+                    Toast.makeText(MainActivity.this, "Data Updated", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Data Not Updated", Toast.LENGTH_SHORT).show();
                 }
-                else {
-                    Toast.makeText(MainActivity.this, "Something error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer deleteData= myDB.deleteData(txt_id.getText().toString().trim());
+
+                if(deleteData>0){
+                    Toast.makeText(MainActivity.this, "Data Deleted", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Data Not Deleted", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+    }
+
+    private void show(String title, String Message) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 }
